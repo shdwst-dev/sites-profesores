cbb180b861d18364935e50b68123f4d4b174c008
shdwst <124049686@upq.edu.mx>
2026-01-12 12:23:23 -0600

    feat: agregar logo UPQ y simplificar contenido en la página de inicio

--- sites-profesores/app/page.tsx
+++ sites-profesores/app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import { Monitor, Network } from "lucide-react";
 
 export default function Home() {
+
   return (
     <>
-      <button>dsadadadsadasd</button>
-      <p>Esto es un parrafo</p>
+    <p>ola</p>
     </>
   );
 }
